173e1f2335387962fc87d5351ae038cdb47e2452
Muhammad Rafly El Quraish <m.rafly@kuncicoin.com>
2025-08-06 02:31:14 +0700

    feat: wa bot floating bubble

--- laravel/resources/views/vendor/adminlte/master-auth.blade.php
+++ laravel/resources/views/vendor/adminlte/master-auth.blade.php
@@ -24,18 +24,18 @@
 
     {{-- Base Stylesheets --}}
     @if(!config('adminlte.enabled_laravel_mix'))
-        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
 
-        @if(config('adminlte.google_fonts.allowed', true))
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-        @endif
+    @if(config('adminlte.google_fonts.allowed', true))
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    @endif
     @else
-        <link rel="stylesheet" href="{{ mix(config('adminlte.laravel_mix_css_path', 'css/app.css')) }}">
+    <link rel="stylesheet" href="{{ mix(config('adminlte.laravel_mix_css_path', 'css/app.css')) }}">
     @endif
 
     {{-- Extra Configured Plugins Stylesheets --}}
@@ -43,11 +43,11 @@
 
     {{-- Livewire Styles --}}
     @if(config('adminlte.livewire'))
-        @if(intval(app()->version()) >= 7)
-            @livewireStyles
-        @else
-            <livewire:styles />
-        @endif
+    @if(intval(app()->version()) >= 7)
+    @livewireStyles
+    @else
+    <livewire:styles />
+    @endif
     @endif
 
     {{-- Custom Stylesheets (post AdminLTE) --}}
@@ -55,25 +55,25 @@
 
     {{-- Favicon --}}
     @if(config('adminlte.use_ico_only'))
-        <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" />
+    <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" />
     @elseif(config('adminlte.use_full_favicon'))
-        <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" />
-        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicons/android-icon-192x192.png') }}">
-        <link rel="manifest" crossorigin="use-credentials" href="{{ asset('favicons/manifest.json') }}">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="{{ asset('favicon/ms-icon-144x144.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/android-icon-192x192.png') }}">
+    <link rel="manifest" crossorigin="use-credentials" href="{{ asset('favicons/manifest.json') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('favicon/ms-icon-144x144.png') }}">
     @endif
 
 </head>
@@ -85,7 +85,7 @@
             width: 100vw;
             overflow: hidden;">
     <div
-    style=" display:flex;
+        style=" display:flex;
             width: 100vw;
             height: 500vh;
             background: rgb(212,236,240);
@@ -99,48 +99,50 @@
             box-shadow: rgba(0, 0, 0, 0.6) 0px 14px 50px, rgba(0, 0, 0, 0.2) 0px 10px 10px;">
     </div>
     <div class="d-flex flex-column justify-content-center w-50"></div>
-        <div style="z-index: 10; position:absolute; top: 30%; left:10%; width: 400px; height: auto;" class="d-flex flex-column justify-content-center align-items-center">
-            <div class="{{ $auth_type ?? 'login' }}-logo">
-                <a href="{{ $dashboard_url }}">
-                    @if (config('adminlte.auth_logo.enabled', false))
-                        <img src="{{ asset(config('adminlte.auth_logo.img.path')) }}"
-                             alt="{{ config('adminlte.auth_logo.img.alt') }}"
-                             @if (config('adminlte.auth_logo.img.class', null))
-                                class="{{ config('adminlte.auth_logo.img.class') }}"
-                             @endif
-                             @if (config('adminlte.auth_logo.img.width', null))
-                                width="{{ config('adminlte.auth_logo.img.width') }}"
-                             @endif
-                             @if (config('adminlte.auth_logo.img.height', null))
-                                height="{{ config('adminlte.auth_logo.img.height') }}"
-                             @endif>
-                    @else
-                        <img src="{{ asset(config('adminlte.logo_img')) }}"
-                             alt="{{ config('adminlte.logo_img_alt') }}" height="50">
+    <div style="z-index: 10; position:absolute; top: 30%; left:10%; width: 400px; height: auto;" class="d-flex flex-column justify-content-center align-items-center">
+        <div class="{{ $auth_type ?? 'login' }}-logo">
+            <a href="{{ $dashboard_url }}">
+                @if (config('adminlte.auth_logo.enabled', false))
+                <img src="{{ asset(config('adminlte.auth_logo.img.path')) }}"
+                    alt="{{ config('adminlte.auth_logo.img.alt') }}"
+                    @if (config('adminlte.auth_logo.img.class', null))
+                    class="{{ config('adminlte.auth_logo.img.class') }}"
                     @endif
+                    @if (config('adminlte.auth_logo.img.width', null))
+                    width="{{ config('adminlte.auth_logo.img.width') }}"
+                    @endif
+                    @if (config('adminlte.auth_logo.img.height', null))
+                    height="{{ config('adminlte.auth_logo.img.height') }}"
+                    @endif>
+                @else
+                <img src="{{ asset(config('adminlte.logo_img')) }}"
+                    alt="{{ config('adminlte.logo_img_alt') }}" height="50">
+                @endif
 
-                    {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
 
-                </a>
-            </div>
-            <a href="/home" class="text-center pt-2 text-dark link-dark" style="font-family: 'Sora', sans-serif; font-size: 26px;">
-                Sistem Informasi Layanan<br>Fakultas Sains dan Teknologi
+                {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+
             </a>
         </div>
+        <a href="/home" class="text-center pt-2 text-dark link-dark" style="font-family: 'Sora', sans-serif; font-size: 26px;">
+            Sistem Informasi Layanan<br>Fakultas Sains dan Teknologi
+        </a>
+    </div>
     </div>
     {{-- Body Content --}}
     <div class="d-flex column justify-content-center align-items-center w-50 pt-5">
         @yield('body')
+
     </div>
 
     {{-- Base Scripts --}}
     @if(!config('adminlte.enabled_laravel_mix'))
-        <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-        <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
     @else
-        <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
+    <script src="{{ mix(config('adminlte.laravel_mix_js_path', 'js/app.js')) }}"></script>
     @endif
 
     {{-- Extra Configured Plugins Scripts --}}
@@ -148,15 +150,47 @@
 
     {{-- Livewire Script --}}
     @if(config('adminlte.livewire'))
-        @if(intval(app()->version()) >= 7)
-            @livewireScripts
-        @else
-            <livewire:scripts />
-        @endif
+    @if(intval(app()->version()) >= 7)
+    @livewireScripts
+    @else
+    <livewire:scripts />
+    @endif
     @endif
 
     {{-- Custom Scripts --}}
     @yield('adminlte_js')
+
+    <!-- ChatBot TI -->
+    <a href="https://wa.me/628983636919?text=Hi" target="_blank" id="whatsapp-button" aria-label="Chat on WhatsApp">
+        <img src="{{ asset(config('adminlte.icon_wa', 'vendor/adminlte/dist/img/wa-bot.png')) }}" alt="WhatsApp" />
+    </a>
+
+    <style>
+        #whatsapp-button {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            z-index: 1000;
+            background-color: #25d366;
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: transform 0.3s ease;
+        }
+
+        #whatsapp-button:hover {
+            transform: scale(1.1);
+        }
+
+        #whatsapp-button img {
+            width: 30px;
+            height: 30px;
+        }
+    </style>
 
 </body>
 
